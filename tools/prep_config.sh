6e122b198fb71303577a98c50384fb64530eea9d
nhayden <nhayden@fredhutch.org>
2015-02-18 10:30:07 -0800

    almost-working configure.win approach

---
+++ tools/prep_config.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+R_CC32="gcc -m32"
+R_CPP32="gcc -m32 -E"
+R_CC64="gcc -m64"
+R_CPP64="gcc -m64 -E"
+
+##R_BIN="${1:-`which R`}"
+##R_DOS=`echo "$R_BIN" | unix2dos`
+##echo "$R_DOS"
+#R_BIN=`echo "$R_BIN" | sed 's| |\\\ |'`
+
+#R_CC=`${R_BIN} CMD config CC`
+
+##echo $R_CC
