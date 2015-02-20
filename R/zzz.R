c1386b9c00220a9f768bfdef30922fcd064a2cb0
nhayden <nhayden@fredhutch.org>
2015-02-20 14:23:48 -0800

    update so packages linking to Rhtslib can rely on pkgconfig

--- R/zzz.R
+++ R/zzz.R
@@ -13,11 +13,12 @@ pkgconfig <-
         sprintf('-I"%s"', system.file("include", package="Rhtslib"))
     }, PKG_LIBS={
         switch(Sys.info()['sysname'], Linux={
-            sprintf('-L"%s" -Wl,-rpath,"%s" -lhts -lz', patharch, patharch)
+            sprintf('-L"%s" -Wl,-rpath,"%s" -lhts -lz -lpthread', patharch, patharch)
         }, Darwin={
-            sprintf('-L"%s" -lhts -lz', patharch)
+            sprintf('-L"%s" -lhts -lz -lpthread', patharch)
         }, Windows={
-            sprintf('-L"%s" -lhts', patharch)
+            sprintf('-L"%s" -lhts -lpthread -lws2_32 %s', patharch,
+		        capture.output(zlibbioc:::pkgconfig("PKG_LIBS_static")))
         }
     )})
 
